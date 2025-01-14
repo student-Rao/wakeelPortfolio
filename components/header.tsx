1e0395babc8f62aa02ec97ff930e763a1c53cb7f
Rao Wakeel <160309352+student-Rao@users.noreply.github.com>
2025-01-14 09:01:11 +0500

    Your commit message here

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image"
-import portfoliologo from "../images/portfoliologo.png"
+// import Image from "next/image"
+// import portfoliologo from "../images/portfoliologo.png"
 import Link from "next/link"
 export default function Header() {
     return (
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
-import pic from '../../images/pic.png'
-import { link } from "fs";
+// import pic from '../../images/pic.png'
+// import { link } from "fs";
 export default function Home() {
   return (
     
